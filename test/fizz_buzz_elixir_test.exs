7f2fa17a792b21f160651d00b3e613606f6c133c
Weerasak Chongnguluam <singpor@gmail.com>
2016-04-01 17:13:40 +0700

    Add FizzBuzzElixir.say test case 1 shoud be 1

--- test/fizz_buzz_elixir_test.exs
+++ test/fizz_buzz_elixir_test.exs
@@ -2,7 +2,7 @@ defmodule FizzBuzzElixirTest do
   use ExUnit.Case
   doctest FizzBuzzElixir
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "FizzBuzzElixir.say 1 should eqaul 1" do
+    assert FizzBuzzElixir.say(1) == "1"
   end
 end
