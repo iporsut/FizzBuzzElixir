91da2cc78541c921e0a8458363488d28b64b1a2f
Weerasak Chongnguluam <singpor@gmail.com>
2016-04-02 19:30:00 +0700

    Refactor to use pipe and anonymouse function

--- lib/fizz_buzz_elixir.ex
+++ lib/fizz_buzz_elixir.ex
@@ -1,9 +1,19 @@
 defmodule FizzBuzzElixir do
+
 	def say(num) do
-		case num do
-      1 -> "1"
-      2 -> "2"
-      3 -> "Fizz"
-		end
+    (fizz num) <> (buzz num) |> (empty_then num).()
 	end
+
+  def fizz(num) do
+    if rem(num,3) === 0, do: "Fizz", else: ""
+  end
+
+  def buzz(num) do
+    if rem(num,5) === 0, do: "Buzz", else: ""
+  end
+
+  def empty_then(v) do
+    fn s -> if s === "", do: Integer.to_string(v), else: s end
+  end
+
 end
--- test/fizz_buzz_elixir_test.exs
+++ test/fizz_buzz_elixir_test.exs
@@ -14,4 +14,32 @@ defmodule FizzBuzzElixirTest do
     assert FizzBuzzElixir.say(3) == "Fizz"
   end
 
+  test "FizzBuzzElixir.say 4 should eqaul 4" do
+    assert FizzBuzzElixir.say(4) == "4"
+  end
+
+  test "FizzBuzzElixir.say 5 should eqaul Buzz" do
+    assert FizzBuzzElixir.say(5) == "Buzz"
+  end
+
+  test "FizzBuzzElixir.say 6 should eqaul Fizz" do
+    assert FizzBuzzElixir.say(6) == "Fizz"
+  end
+
+  test "FizzBuzzElixir.say 9 should eqaul Fizz" do
+    assert FizzBuzzElixir.say(9) == "Fizz"
+  end
+
+  test "FizzBuzzElixir.say 10 should eqaul Buzz" do
+    assert FizzBuzzElixir.say(10) == "Buzz"
+  end
+
+  test "FizzBuzzElixir.say 15 should eqaul FizzBuzz" do
+    assert FizzBuzzElixir.say(15) == "FizzBuzz"
+  end
+
+  test "FizzBuzzElixir.say 30 should eqaul FizzBuzz" do
+    assert FizzBuzzElixir.say(30) == "FizzBuzz"
+  end
+
 end
